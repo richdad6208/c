dbb8d1202a639827d34329294824d0a03e39a36e
Jaesung Park <126126067+richdad6208@users.noreply.github.com>
2025-05-04 11:20:33 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, Robot!\n");
+    char FIRST_NAME[] = "Jone"; 
+    char LAST_NAME[] = "Doe";
+    char name[]; 
+
+    name = strncat(name, FIRST_NAME, 20); 
+    name = strncat(name, LAST_NAME, 20); 
+    
+    printf("$s\n", name);
     return 0; 
 }
