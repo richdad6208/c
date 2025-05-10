72b0656edaf3b2a5fa1068822d20f5ca695b87b4
Jaesung Park <richdad6208@gmail.com>
2025-05-10 22:44:39 +0900

    hjg

--- hello.c
+++ hello.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main(void) {
-    double d = 85.4; 
-    int score = (int) d; 
+    int input, answer;
 
-    printf("score=%d\n", score); 
-    printf("d=%f\n", d); 
+    srand((unsigned) time(NULL)); 
+    answer = rand() % 100 + 1; 
 
-    return 0; 
+    do {
+        printf("1과 100 사이의 정수를 입력해주세요. >");
+        scanf("%d", &input);
+
+        if (input > answer) {
+            printf("더 작은 수를 입력해주세요\n");
+        } else {
+            printf("더 큰 수를 입력해주세요.\n");
+        }
+    } while (input != answer);
+
+    printf("정답입니다. \n");
+
+    return 0;
 }
-
