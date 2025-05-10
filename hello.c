0f8e702c734fdb1107e8fb0f7f2944f4ab0fd6dd
Jaesung Park <richdad6208@gmail.com>
2025-05-10 17:53:52 +0900

    dd

--- hello.c
+++ hello.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
-#include <float.h>
-
-typedef union {
-    int hex;
-    float dec;
-} int_float;
 
 int main(void) {
-    int_float data, data2; 
+    double d = 85.4; 
+    int score = (int) d; 
 
-    data.dec = 9.1234567f;
-    data2.hex = 0x4111F9AD;
-
-    printf("hex=%08X\t dec=%20.18f\n", data.hex, data.dec); 
-    printf("hex=%08X\t dec=%20.18f\n", data2.hex, data2.dec);
-
-    printf("FLT_ROUNDS=%d\n", FLT_ROUNDS); 
-    printf("FLT_EPSILON=%11.10f\n", FLT_EPSILON); 
+    printf("score=%d\n", score); 
+    printf("d=%f\n", d); 
 
     return 0; 
 }
+
